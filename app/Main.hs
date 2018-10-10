8f4f9c5a194ec9ab5d113036227cba81aaa34432
Jan Hrček <jhrcek@redhat.com>
2018-10-10 19:32:12 +0200

    Parse test duration from junit results (closes #1)

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Console.Checks.JunitReport (readInfos, tiDuration)
 import Console.Checks.MavenDownload (getDistinctDownloadUrls,
                                      getMavenDownloadDurations)
 import Console.Checks.MavenPlugin (pluginDurations)
 import Console.Parse (parseTimestamps)
 import Console.Types (diffElapsed, getInterval, getLogLine)
+import Data.List (sortOn)
 import qualified Data.Set as Set
 import qualified Data.Text.IO as T
 
 main :: IO ()
 main = do
     log_ts <- parseTimestamps =<< T.readFile "consoleText_timestamps"
+    section
     putStrLn $ "Total duration: " <> show (uncurry diffElapsed $ getInterval log_ts)
+    section
     putStrLn $ "Time spent downloading: " <> show (mconcat $ getMavenDownloadDurations log_ts)
+
     let urls = getDistinctDownloadUrls $ fmap getLogLine log_ts
     putStrLn $ "Number of distinct download URLs: " <> show (Set.size urls)
     mapM_ print . take 10 . reverse $ pluginDurations log_ts
+
+    testInfos <- readInfos "junitreport"
+    let testsOver10Seconds = length . takeWhile (\testInfo -> tiDuration testInfo > 10) . reverse  $ sortOn tiDuration testInfos
+    section
+    putStrLn $ "Number of test methods that take over 10 seconds to run: " <> show testsOver10Seconds
+
+section :: IO ()
+section = putStrLn (replicate 80 '-')
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,29 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
 module Main where
 
+import Console.Checks.JunitReport (TestInfo (..))
 import Console.Parse (parseLine)
 import Console.Types
+import qualified Data.Aeson as Aeson
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-    describe "Console.Parse" $ do
-        describe "parseLine" $ do
-            it "should parse lines where maven download starts" $ do
-                parseLine "[INFO] Downloading from mirror-central: http://some.url/1"
-                  `shouldBe` MavenTransferStart Download (RepoName "mirror-central") (RepoUrl "http://some.url/1")
-            it "should parse lines where maven download ends" $ do
-                parseLine "[INFO] Downloaded from mirror-central: http://some.url/2 (15 B at 30 MB/s)"
-                  `shouldBe` MavenTransferEnd Download (RepoName "mirror-central") (RepoUrl "http://some.url/2") (FileSize 15 B) (Just (TransferSpeed 30 MB))
-            it "should parse lines where maven upload starts" $ do
-                parseLine "[INFO] Uploading to local: http://some.url/3"
-                  `shouldBe` MavenTransferStart Upload (RepoName "local") (RepoUrl "http://some.url/3")
-            it "should parse lines where maven upload ends" $ do
-                parseLine "[INFO] Uploaded to local: http://some.url/4 (368 B at 368 kB/s)"
-                  `shouldBe` MavenTransferEnd Upload (RepoName "local") (RepoUrl "http://some.url/4") (FileSize 368 B) (Just (TransferSpeed 368 KB))
-            it "should parse lines where maven upload ends - without transfer speed info" $ do
-                parseLine "[INFO] Uploaded to local: http://some.url/5 (1.9 kB)"
-                  `shouldBe` MavenTransferEnd Upload (RepoName "local") (RepoUrl "http://some.url/5") (FileSize 1.9 KB) Nothing
-            it "should parse lines where maven plugin execution starts" $ do
-                parseLine "[INFO] --- buildnumber-maven-plugin:1.4:create (get-scm-revision) @ uberfire-project-client ---"
-                  `shouldBe` MavenPluginExecution (PluginExecution "buildnumber-maven-plugin" "1.4" "create" "get-scm-revision" "uberfire-project-client")
+  describe "Console.Parse" $ do
+    describe "parseLine" $ do
+      it "should parse lines where maven download starts" $ do
+        parseLine "[INFO] Downloading from mirror-central: http://some.url/1"
+          `shouldBe` MavenTransferStart Download (RepoName "mirror-central") (RepoUrl "http://some.url/1")
+      it "should parse lines where maven download ends" $ do
+        parseLine "[INFO] Downloaded from mirror-central: http://some.url/2 (15 B at 30 MB/s)"
+          `shouldBe` MavenTransferEnd Download (RepoName "mirror-central") (RepoUrl "http://some.url/2") (FileSize 15 B) (Just (TransferSpeed 30 MB))
+      it "should parse lines where maven upload starts" $ do
+        parseLine "[INFO] Uploading to local: http://some.url/3"
+          `shouldBe` MavenTransferStart Upload (RepoName "local") (RepoUrl "http://some.url/3")
+      it "should parse lines where maven upload ends" $ do
+        parseLine "[INFO] Uploaded to local: http://some.url/4 (368 B at 368 kB/s)"
+          `shouldBe` MavenTransferEnd Upload (RepoName "local") (RepoUrl "http://some.url/4") (FileSize 368 B) (Just (TransferSpeed 368 KB))
+      it "should parse lines where maven upload ends - without transfer speed info" $ do
+        parseLine "[INFO] Uploaded to local: http://some.url/5 (1.9 kB)"
+          `shouldBe` MavenTransferEnd Upload (RepoName "local") (RepoUrl "http://some.url/5") (FileSize 1.9 KB) Nothing
+      it "should parse lines where maven plugin execution starts" $ do
+        parseLine "[INFO] --- buildnumber-maven-plugin:1.4:create (get-scm-revision) @ uberfire-project-client ---"
+          `shouldBe` MavenPluginExecution (PluginExecution "buildnumber-maven-plugin" "1.4" "create" "get-scm-revision" "uberfire-project-client")
+  describe "Console.Checks.JunitReport" $ do
+      describe "TestInfo" $ do
+        it "should parse single TestInfo JSON object" $
+          Aeson.decode @TestInfo "{\"className\":\"org.dashbuilder.navigation.service.LayoutTemplateAnalyzerTest\",\"duration\":0.002,\"name\":\"testPerspectiveReuseNoRecursiveIssue\"}"
+            `shouldBe` (Just $ TestInfo "org.dashbuilder.navigation.service.LayoutTemplateAnalyzerTest" "testPerspectiveReuseNoRecursiveIssue" 0.002)
