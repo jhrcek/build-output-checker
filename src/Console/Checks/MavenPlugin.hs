5e4ec5fc1fce7c2d1b8bd13f503a91e06b564347
Jan Hrček <jhrcek@redhat.com>
2018-10-11 17:10:26 +0200

    Fix incomplete pattern match in MavenPlugin

--- src/Console/Checks/MavenPlugin.hs
+++ src/Console/Checks/MavenPlugin.hs
@@ -2,18 +2,19 @@
 module Console.Checks.MavenPlugin (pluginDurations) where
 import Console.Types
 import Data.List (sortOn)
+import Data.Maybe (mapMaybe)
 
 pluginDurations :: [TimedLogLine] -> [(PluginExecution, Duration)]
 pluginDurations linez =
-    sortOn (\(_,duration) -> duration) $ zipWith calcDuration pluginStarts (getElapsedTime <$> tail pluginStarts)
+    sortOn (\(_,duration) -> duration) $ zipWith calcDuration pluginStarts (tail pluginStarts)
   where
-    pluginStarts = filter isMavenPluginExecution linez
+    pluginStarts = mapMaybe getPluginExecution linez
 
-calcDuration :: TimedLogLine -> ElapsedTime -> (PluginExecution, Duration)
-calcDuration (TimedLogLine tsStart (MavenPluginExecution ple)) timestampOfNextPluginStart
-    = (ple, diffElapsed tsStart timestampOfNextPluginStart)
+calcDuration :: (ElapsedTime, PluginExecution) -> (ElapsedTime, PluginExecution) -> (PluginExecution, Duration)
+calcDuration (start1, pl1) (start2, _)
+    = (pl1, diffElapsed start1 start2)
 
-isMavenPluginExecution :: TimedLogLine -> Bool
-isMavenPluginExecution = \case
-    (TimedLogLine _ (MavenPluginExecution _)) -> True
-    _ -> False
+getPluginExecution :: TimedLogLine -> Maybe (ElapsedTime, PluginExecution)
+getPluginExecution = \case
+    (TimedLogLine et (MavenPluginExecution ple)) -> Just (et, ple)
+    _                                            -> Nothing
